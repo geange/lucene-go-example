78bb0ea7da57df988c8d3b189db367bfdc7f2ea1
geange <geange@outlook.com>
2023-07-16 23:52:23 +0800

    fix: 优化util/automaton/main.go

--- util/automaton/main.go
+++ util/automaton/main.go
@@ -1,16 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
-	//builder := automaton.NewNewBuilder()
-	//
-	//s1 := builder.CreateState()
-	//s2 := builder.CreateState()
-	//s3 := builder.CreateState()
-	//s4 := builder.CreateState()
-	//s5 := builder.CreateState()
-	//s6 := builder.CreateState()
-	//
-	//builder.AddTransitionLabel(s1, s2, 1)
-	//builder.AddTransitionLabel(s1, s5, 2)
+	"github.com/geange/lucene-go/core/util/automaton"
+)
+
+func main() {
+	auto := automaton.NewAutomaton()
+
+	state1 := auto.CreateState()
+	state2 := auto.CreateState()
+	state3 := auto.CreateState()
+	state4 := auto.CreateState()
+
+	auto.AddTransitionLabel(state1, state2, int(1))
+	auto.AddTransitionLabel(state1, state3, int(2))
+	auto.AddTransitionLabel(state2, state4, int(3))
+	auto.AddTransitionLabel(state3, state4, int(4))
+
+	state := auto.GetNumTransitionsWithState(state1)
+
+	step := auto.Step(state1, int(1))
+	if step != state2 {
+		log.Panicf("step = %d", step)
+	}
+	fmt.Println(state)
 }
